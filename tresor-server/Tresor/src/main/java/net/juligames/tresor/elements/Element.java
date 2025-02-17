2aa884ab29d9c8e0805201b9ee1f1463ad82189b
Ture Bentzin <ture.bentzin@alumni.fh-aachen.de>
2025-02-17 13:54:26 +0100

    migrate to abstract class

--- tresor-server/Tresor/src/main/java/net/juligames/tresor/elements/Element.java
+++ tresor-server/Tresor/src/main/java/net/juligames/tresor/elements/Element.java
@@ -4,6 +4,7 @@ package net.juligames.tresor.elements;
 import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.terminal.Terminal;
 import net.juligames.tresor.TresorGUI;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Range;
@@ -14,33 +15,77 @@ import java.io.IOException;
  * @author Ture Bentzin
  * @since 17-02-2025
  */
-public interface Element {
+public abstract class Element {
+
+
+    private final @NotNull TerminalPosition position;
+    private final @NotNull TerminalSize size;
+
+    public Element(@NotNull TerminalPosition position, @NotNull TerminalSize size) {
+        this.position = position;
+        this.size = size;
+    }
 
     /**
      * Get the position of the element.
+     *
      * @return the position of the element.
      */
-    @NotNull TerminalPosition getPosition();
+    @NotNull TerminalPosition getPosition() {
+        return position;
+    }
 
-    @NotNull TerminalSize getSize();
+    @NotNull TerminalSize getSize() {
+        return size;
+    }
 
     @Range(from = 0, to = Integer.MAX_VALUE)
-    default int getWidth() {
+    final int getWidth() {
         return getSize().getColumns();
     }
 
     @Range(from = 0, to = Integer.MAX_VALUE)
-    default int getHeight() {
+    final int getHeight() {
         return getSize().getRows();
     }
 
+    final @NotNull TerminalPosition getBottomRight() {
+        return getPosition().withRelative(getWidth(), getHeight());
+    }
+
+    final @NotNull TerminalPosition getTopRight() {
+        return getPosition().withRelative(getWidth(), 0);
+    }
+
+    final @NotNull TerminalPosition getBottomLeft() {
+        return getPosition().withRelative(0, getHeight());
+    }
+
+
+
+    //Maybe: @implNote This method might be called from different threads for multiple elements. Implementations should be thread-safe.
+
+
     /**
      * Draw the element on the screen.
+     *
      * @return if the element was drawn successfully. If false, the element was not drawn and this method promises that no changes were made to the screen.
      * @throws IOException if an I/O error occurs. The element may have been partially drawn.
-     * @implNote This method might be called from different threads for multiple elements. Implementations should be thread-safe.
      */
-    boolean draw() throws IOException;
+    abstract boolean draw(final @NotNull TresorGUI gui) throws IOException;
+
+
+    final boolean drawBorder(int thickness, char border, @NotNull TextGraphics section) {
+        TerminalPosition topLeft = getPosition();
+        TerminalPosition bottomRight = getBottomRight();
+
+        for (int i = 0; i < thickness; i++) {
+            section.drawLine(topLeft.withRelative(i, i), bottomRight.withRelative(-i, -i), border);
+            section.drawLine(topLeft.withRelative(i, i), bottomRight.withRelative(-i, -i), border);
+        }
+
+        return true;
+    }
 
 
 
