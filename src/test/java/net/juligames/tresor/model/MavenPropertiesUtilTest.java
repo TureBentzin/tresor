e147228d5ee08423b06d042c2424eb4e4e7b0f6d
Ture Bentzin <ture.bentzin@alumni.fh-aachen.de>
2025-02-21 22:59:45 +0100

    fix empty properties

--- src/test/java/net/juligames/tresor/model/MavenPropertiesUtilTest.java
+++ src/test/java/net/juligames/tresor/model/MavenPropertiesUtilTest.java
@@ -22,6 +22,7 @@ class MavenPropertiesUtilTest {
         log.info("ArtifactId: {}", artifactId);
         assertNotNull(artifactId, "ArtifactId is null");
         assertFalse(artifactId.isEmpty(), "ArtifactId is empty");
+        assertNotEquals("unknown", artifactId, "ArtifactId is unknown");
     }
 
 
@@ -31,6 +32,7 @@ class MavenPropertiesUtilTest {
         log.info("GitCommit: {}", gitCommit);
         assertNotNull(gitCommit, "Unable to find current git commit");
         assertFalse(gitCommit.isEmpty(), "Unable to find current git commit");
+        assertNotEquals("unknown", gitCommit, "GitCommit is unknown");
     }
 
     @Test
@@ -39,6 +41,7 @@ class MavenPropertiesUtilTest {
         log.info("GitBranch: {}", gitBranch);
         assertNotNull(gitBranch, "Unable to find current git branch");
         assertFalse(gitBranch.isEmpty(), "Unable to find current git branch");
+        assertNotEquals("unknown", gitBranch, "GitBranch is unknown");
     }
 
     @Test
@@ -47,6 +50,7 @@ class MavenPropertiesUtilTest {
         log.info("Version: {}", version);
         assertNotNull(version, "Unable to find current version");
         assertFalse(version.isEmpty(), "Unable to find current version");
+        assertNotEquals("unknown", version, "Version is unknown");
     }
 
     @Test
@@ -55,6 +59,7 @@ class MavenPropertiesUtilTest {
         log.info("BuildTime: {}", buildTime);
         assertNotNull(buildTime, "Unable to find build time");
         assertFalse(buildTime.isEmpty(), "Unable to find build time");
+        assertNotEquals("unknown", buildTime, "BuildTime is unknown");
     }
 
     @Test
@@ -63,6 +68,7 @@ class MavenPropertiesUtilTest {
         log.info("User Email: {}", buildUserEmail);
         assertNotNull(buildUserEmail, "Git user email not set");
         assertFalse(buildUserEmail.isEmpty(), "Git user email not set");
+        assertNotEquals("unknown", buildUserEmail, "User Email is unknown");
     }
 
     @Test
@@ -71,6 +77,7 @@ class MavenPropertiesUtilTest {
         log.info("User Name: {}", buildUserName);
         assertNotNull(buildUserName, "Git user name not set");
         assertFalse(buildUserName.isEmpty(), "Git user name not set");
+        assertNotEquals("unknown", buildUserName, "User Name is unknown");
     }
 
 
