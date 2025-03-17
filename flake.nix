7d7d48f06f95e2e2910ca1bdcd4751e797d8accd
Ture Bentzin <ture.bentzin@alumni.fh-aachen.de>
2025-03-17 10:37:38 +0000

    fix jdk

--- flake.nix
+++ flake.nix
@@ -3,31 +3,36 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    
   };
 
-  outputs = { self, nixpkgs, jdk32, makeWrapper, maven }:
+  outputs = { self, nixpkgs, makeWrapper, maven }:
   let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    buildInputs = [];
+    jdk23 = pkgs.jdk23;
   in {
     packages.x86_64-linux.default = maven.buildMavenPackage rec {
-  pname = "tresor";
-  version = "1.0-SNAPSHOT";
+      pname = "tresor";
+      version = "1.0-SNAPSHOT";
 
-  src = "./";
-  
-  mvnHash = "";
+      src = "./";
 
-  nativeBuildInputs = [ makeWrapper ];
+      mvnHash = "";
 
-  installPhase = ''
-    mkdir -p $out/bin $out/share/tresor
-    install -Dm644 ./target/Tresor-1.0-SNAPSHOT.jar $out/share/tresor
+      nativeBuildInputs = [ makeWrapper ];
 
-    makeWrapper ${jdk32}/bin/java $out/bin/tresor \
-      --add-flags "-jar $out/share/tresor/Tresor-1.0-SNAPSHOT.jar"
-  '';
+      installPhase = ''
+        mkdir -p $out/bin $out/share/tresor
+        install -Dm644 ./target/Tresor-1.0-SNAPSHOT.jar $out/share/tresor
+
+        makeWrapper ${jdk23}/bin/java $out/bin/tresor \
+          --add-flags "-jar $out/share/tresor/Tresor-1.0-SNAPSHOT.jar"
+      '';
+    };
+
+    # Add a development shell with nixpkgs-fmt
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = [ pkgs.nixpkgs-fmt ];
+    };
   };
-};
 }
+
