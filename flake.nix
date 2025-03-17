4a356a86478804ca2b50e65ab3612a5555b2f7ad
Ture Bentzin <ture.bentzin@alumni.fh-aachen.de>
2025-03-17 10:29:50 +0000

    start flake rewrite

--- flake.nix
+++ flake.nix
@@ -1,19 +1,33 @@
 {
-  description = "Tresor Server - TELNET Gui banking for steamcoin2api";
+  description = "TresorGUI Telnet Server";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, jdk32, makeWrapper, maven }:
   let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in
-  {
-    packages.${system} = {
-        tresor-server = pkgs.callPackage ./package.nix { };
-        default = self.packages.${system}.tresor-server;
-    };
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    buildInputs = [];
+  in {
+    packages.x86_64-linux.default = maven.buildMavenPackage rec {
+  pname = "tresor";
+  version = "1.0-SNAPSHOT";
+
+  src = "./";
+  
+  mvnHash = "";
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  installPhase = ''
+    mkdir -p $out/bin $out/share/tresor
+    install -Dm644 ./target/Tresor-1.0-SNAPSHOT.jar $out/share/tresor
+
+    makeWrapper ${jdk32}/bin/java $out/bin/tresor \
+      --add-flags "-jar $out/share/tresor/Tresor-1.0-SNAPSHOT.jar"
+  '';
   };
+};
 }
